2dcc4a59ebf65d92b58eaa15caa77daefc3ab14e
James Murphy <jamesivdeveloper@gmail.com>
2022-01-09 20:38:15 -0600

    variables for password criteria

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,20 @@
 // Assignment code here
+var generatePassword = function() {
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "0123456789";
+  var length = "";
 
+  // confirm prompts
+
+  var lowerCaseConfirm = window.confirm("Do you want to include lowercase letters?");
+
+  var upperCaseConfirm = window.confirm("Do you want to include upper case letters?");
+
+  var numericConfirm = window.confirm("Do you want to include numbers?");
+
+  var specialConfirm = window.confirm("Do you want to include special characters?")
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,8 +25,9 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword) {
+  writePassword();
+};
